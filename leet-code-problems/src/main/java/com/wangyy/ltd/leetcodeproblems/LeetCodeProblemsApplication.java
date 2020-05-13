d16be4e3bb713185aae4eb5c888d1c7cb1f00f58
‘wangyy-git’ <1152989909@qq.com>
2020-05-13 10:12:35 +0800

    搭建项目

---
+++ leet-code-problems/src/main/java/com/wangyy/ltd/leetcodeproblems/LeetCodeProblemsApplication.java
@@ -0,0 +1,13 @@
+package com.wangyy.ltd.leetcodeproblems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetCodeProblemsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeetCodeProblemsApplication.class, args);
+    }
+
+}
---
+++ leet-code-problems/src/test/java/com/wangyy/ltd/leetcodeproblems/LeetCodeProblemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wangyy.ltd.leetcodeproblems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetCodeProblemsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
