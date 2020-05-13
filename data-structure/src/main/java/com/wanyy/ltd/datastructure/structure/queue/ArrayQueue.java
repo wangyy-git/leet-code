8a83519f3c47415c6a630459146dbe45d1d47323
‘wangyy-git’ <1152989909@qq.com>
2020-05-13 22:31:38 +0800

    基于数组的队列的实现

--- data-structure/src/main/java/com/wanyy/ltd/datastructure/structure/queue/ArrayQueue.java
+++ data-structure/src/main/java/com/wanyy/ltd/datastructure/structure/queue/ArrayQueue.java
@@ -2,37 +2,45 @@ package com.wanyy.ltd.datastructure.structure.queue;
 
 import java.util.Arrays;
 
-public class Queue {
+public class ArrayQueue {
 
     
     private int maxSize;
+    //rear指向队列尾部，就是队列的最后一个数据
     private int rear = -1;
+    //front指向的是头部的前一个位置
     private int front = -1;
     private int[] queue;
     
-    public Queue(int maxSize){
+    public ArrayQueue(int maxSize){
         this.maxSize = maxSize;
         this.queue = new int[maxSize];
     }
     
     
-    public Queue addQueue(int value){
-//        System.out.println(rear);
-        ++this.rear;
+    public ArrayQueue addQueue(int value){
+       
         if (this.rear == this.maxSize - 1){
             throw new RuntimeException("存满了");
         }
-        System.out.println("front -> " + front);
-        this.queue[this.rear] = value;
+        this.queue[++rear] = value;
         return this;
     }
 
     public int fetchQueue(){
-        if (this.front == this.rear) throw new RuntimeException("取完了");
+        if (this.front >= this.rear) throw new RuntimeException("取完了");
         return this.queue[++this.front];
     }
 
+    /**
+     * for (int i=0;i<length;i++){
+     *    queue[i] = this.queue[++front];
+     * }
+     * 前期在toString方法这里直接++this.front，导致报错
+     */
     public String toString(){
+        int rear = this.rear;
+        int front = this.front;
         if (rear<0) return "[]";
         int length = rear - front;
         int[] queue = new int[length];
--- data-structure/src/main/java/com/wanyy/ltd/datastructure/structure/queue/QueueTest.java
+++ data-structure/src/main/java/com/wanyy/ltd/datastructure/structure/queue/QueueTest.java
@@ -2,16 +2,16 @@ package com.wanyy.ltd.datastructure.structure.queue;
 
 public class QueueTest {
     public static void main(String[] args) {
-        Queue queue = new Queue(10);
+        ArrayQueue queue = new ArrayQueue(3);
         queue.addQueue(1);
         queue.addQueue(2);
         queue.addQueue(3);
         
-        System.out.println("fetch" + queue.fetchQueue());
+        System.out.println("fetch -> " + queue.fetchQueue());
         System.out.println(queue.toString());
-        System.out.println("fetch" + queue.fetchQueue());
-        System.out.println("fetch" + queue.fetchQueue());
-        System.out.println("fetch" + queue.fetchQueue());
+        System.out.println("fetch -> " + queue.fetchQueue());
+        System.out.println("fetch -> " + queue.fetchQueue());
+//        System.out.println("fetch" + queue.fetchQueue());
 //        queue.addQueue(3);
         System.out.println(queue.toString());
     }
