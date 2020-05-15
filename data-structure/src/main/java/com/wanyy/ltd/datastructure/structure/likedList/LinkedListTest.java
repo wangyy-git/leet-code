b39b1871abc39bffe7541da929c2de3ad77a25b5
‘wangyy-git’ <1152989909@qq.com>
2020-05-15 11:26:53 +0800

    单链表的一些面试题

--- data-structure/src/main/java/com/wanyy/ltd/datastructure/structure/likedList/LinkedListTest.java
+++ data-structure/src/main/java/com/wanyy/ltd/datastructure/structure/likedList/LinkedListTest.java
@@ -1,26 +1,49 @@
 package com.wanyy.ltd.datastructure.structure.likedList;
 
+
+import java.util.Stack;
+
 public class LinkedListTest {
 
+    
     public static void main(String[] args) {
         HeroNode h1 = new HeroNode(1,"影魔");
-        HeroNode h2 = new HeroNode(2,"斧王");
+        HeroNode h2 = new HeroNode(2,"小黑");
         HeroNode h3 = new HeroNode(3,"巨魔");
-        HeroNode h4 = new HeroNode(4,"先知");
+        HeroNode h4 = new HeroNode(4,"剑圣");
+
+        HeroNode h5 = new HeroNode(5,"影魔");
+        HeroNode h6 = new HeroNode(6,"刚被");
+        HeroNode h7 = new HeroNode(7,"屠夫");
+        HeroNode h8 = new HeroNode(8,"混沌");
         SingleLinkedList single = new SingleLinkedList();
-//        single.addNode(h1);
-//        single.addNode(h2);
-//        single.addNode(h3);
-//        single.addNode(h4);
-//        single.iterList();
-//        System.out.println("----------------");
-//        single.deleteNode(h2);
         
         single.addNodeByOrder(h3);
         single.addNodeByOrder(h1);
         single.addNodeByOrder(h4);
         single.addNodeByOrder(h2);
+
+        SingleLinkedList merge = new SingleLinkedList();
+
+        merge.addNodeByOrder(h5);
+        merge.addNodeByOrder(h6);
+        merge.addNodeByOrder(h7);
+        merge.addNodeByOrder(h8);
         
-        single.iterList();
+        SingleLinkedListInterview.mergeOrderLinkedList(single,merge);
+        
+        iteratorByHead(single.headNode);
+//        SingleLinkedListInterview.reversePrintLink(single.headNode);
+        
+        
     }
+    
+    private static void iteratorByHead(HeroNode headNode){
+        HeroNode temp = headNode.next;
+        while (temp != null) {
+            System.out.println(temp);
+            temp = temp.next;
+        }
+    }
+    
 }
--- data-structure/src/main/java/com/wanyy/ltd/datastructure/structure/likedList/SingleLinkedListInterview.java
+++ data-structure/src/main/java/com/wanyy/ltd/datastructure/structure/likedList/SingleLinkedListInterview.java
@@ -1,16 +1,29 @@
 package com.wanyy.ltd.datastructure.structure.likedList;
 
-public class SingleLinkedListInterview {
+import javax.xml.soap.Node;
+import java.util.Stack;
+
+class SingleLinkedListInterview {
     
     /**
      * 获取倒数第K个节点
      */
-    private static void getBackNode(SingleLinkedList linkedList,int k){
+    static void getBackNode(SingleLinkedList linkedList,int k){
         HeroNode headNode = linkedList.headNode;
-        
+        int nodeCount = getNodeCount(linkedList);
+        if (nodeCount < k){
+            System.out.println("链表过短，没有满足条件的节点...");
+        }
+
+        int m = nodeCount - k + 1;
+        HeroNode temp = headNode.next;
+        for (int i = 0;i<nodeCount-k;i++){
+            temp = temp.next;
+        }
+        System.out.println("倒数第 " + k + "个节点为 -> " + temp);
     }
-    
-    private static int getNodeCount(SingleLinkedList linkedList){
+
+    static int getNodeCount(SingleLinkedList linkedList){
         HeroNode temp = linkedList.headNode.next;
         int count = 0;
         while (temp != null){
@@ -19,5 +32,85 @@ public class SingleLinkedListInterview {
         }
         
         return count;
+    }
+    
+    static void reverseLinkedList(HeroNode headNode){
+        //两个next相互赋值  这个就不会改变temp
+        //之前类似将temp直接赋给newNode  导致newNode更改导致temp也发生了更改
+//        newNode.next = temp.next;
+        if (headNode.next == null){
+            System.out.println("链表为空...");
+            return;
+        }
+        if (headNode.next.next == null){
+            System.out.println("链表只有一个节点...");
+            return;
+        }
+        HeroNode current = headNode.next;
+        HeroNode curNext;
+        
+        HeroNode newHeadNode = new HeroNode(0,"");
+        
+        //自己之前没有保存current.next 导致current被重新赋值后其next的丢失
+        //注意此处涉及到内存地址的相互指向
+        //所以要注意赋值的形式
+        while (current != null){
+            
+            curNext = current.next;
+            current.next = newHeadNode.next;
+            newHeadNode.next = current;
+            current = curNext;
+            
+        }
+        
+        headNode.next = newHeadNode.next;
+        
+    }
+
+    /**
+     * 利用Stack的特点 先进后出
+     * 然后打印
+     */
+    static void reversePrintLink(HeroNode headNode){
+        if (headNode.next == null){
+            System.out.println("链表为空");
+            return;
+        }
+
+        Stack<HeroNode> stack = new Stack<>();
+        HeroNode temp = headNode.next;
+        while (temp != null){
+            stack.add(temp);
+            temp = temp.next;
+        }
+        
+        while (!stack.empty()){
+            System.out.println(stack.pop());
+        }
+    }
+
+    /**
+     * 将mergeLink合并到master上 要求仍热有序
+     * @param masterLink    主Link
+     * @param mergeLink     待合并的Link
+     */
+    static void mergeOrderLinkedList(SingleLinkedList masterLink,SingleLinkedList mergeLink){
+        HeroNode mergeHead = mergeLink.headNode;
+        
+        if (mergeHead.next == null){
+            System.out.println("待合并LinkedList为空...");
+            return;
+        }
+        
+        HeroNode mergeCurrent = mergeHead.next;
+        HeroNode mergeCurNext;
+        
+        while (mergeCurrent != null){
+            mergeCurNext = mergeCurrent.next;
+            
+            masterLink.addNodeByOrder(mergeCurrent);
+            
+            mergeCurrent = mergeCurNext;
+        }
     }
 }
