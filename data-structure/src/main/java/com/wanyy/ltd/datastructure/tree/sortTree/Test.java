ece4d9fb331522dc154164a080f1b7ed527b9cb4
wangyy-git <1152989909@qq.com>
2020-06-14 19:23:55 +0800

    break技巧

---
+++ data-structure/src/main/java/com/wanyy/ltd/datastructure/tree/sortTree/Test.java
@@ -0,0 +1,18 @@
+package com.wanyy.ltd.datastructure.tree.sortTree;
+
+public class Test {
+    public static void main(String[] args) {
+        kkkk:
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.println("i=" + i + ",j=" + j);
+                if (j == 1) {
+                    break kkkk;
+                }
+            }
+        }
+
+        System.out.println("---------------");
+    }
+    
+}
